eb0868932b3bab1e425ebfc20faed26d42e162f9
Ludelaplu <lucien.koot@gmail.com>
2019-06-08 09:29:43 +0200

    aanvang python en tevens aanmaken van een notitiefile voor naslag

---
+++ oefening2.py
@@ -0,0 +1,9 @@
+Python 3.6.7 (default, Oct 22 2018, 11:32:17) 
+[GCC 8.2.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Syntactisch correct maar lelijke inspringing .
+x = 1
+if x > 2:
+print ( "x is groter dan 2" )
+else:
+print ( "x is kleiner dan of gelijk aan 2" )
