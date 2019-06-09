a0dd694aff7ceb7b5a5f176fd9153c8f72050ddd
Ludelaplu <lucien.koot@gmail.com>
2019-06-09 22:58:54 +0200

    volume van een kegel

---
+++ blz43.py
@@ -0,0 +1,16 @@
+#hoogte van een kegel
+
+pi = 3.14159265
+straal = 7.5
+hoogte = 8.25
+volume_van_kegel = pi *straal*straal*hoogte/ 3
+print ( volume_van_kegel )
+
+# volume van een kegel van 3 meter hoog
+
+pi = 3.14159265
+straal = 7.5
+hoogte = 3
+volume_van_kegel = pi *straal*straal*hoogte/ 3
+print ( volume_van_kegel )
+
