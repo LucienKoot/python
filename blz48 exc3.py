fa05482f34a1a113cfc2f94e7d652669e2d9a4ec
Ludelaplu <lucien.koot@gmail.com>
2019-06-10 10:39:59 +0200

    excersise 403.

---
+++ blz48 exc3.py
@@ -0,0 +1,28 @@
+CENTEN_IN_EURO = 100
+CENTEN_IN_50CT= 50
+CENTEN_IN_20CT = 20
+CENTEN_IN_DUBBELTJE = 10
+CENTEN_IN_STUIVER = 5
+
+bedrag = 1199
+centen = bedrag
+
+euro       = int( centen / CENTEN_IN_EURO )
+centen -= euro  *  CENTEN_IN_EURO
+50ct        = int(  centen / CENTEN_IN_50CT)
+centen -=euro * CENTEN_IN_50CT
+20ct        = int( centen / CENTEN_IN_20CT )
+centen -= 20ct  *  CENTEN_IN_20CT
+dubbeltjes = int( centen / CENTEN_IN_DUBBELTJE )
+centen -= dubbeltjes  *  CENTEN_IN_DUBBELTJE
+stuivers = int( centen / CENTEN_IN_STUIVER )
+centen -= stuivers  *  CENTEN_IN_STUIVER
+centen = int( centen )
+
+print( bedrag / CENTEN_IN_EURO, "bestaat uit:" )
+print( "Euro:", euro)
+print( "50ct:", 50ct)
+print( "20ct:", 20ct )
+print( "Dubbeltjes:", dubbeltjes )
+print( "Stuivers:", stuivers )
+print( "Centen:", centen )
