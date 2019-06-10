39dcb2c62347faca3838f869111a1026aca57890
Ludelaplu <lucien.koot@gmail.com>
2019-06-10 07:53:56 +0200

    assignment operator.

---
+++ blz46.py
@@ -0,0 +1,17 @@
+# verkorte operatoren
+
+appel = 100
+appel += 3
+print (appel)
+
+appel = 100
+appel -= 3
+print (appel)
+
+appel = 100
+appel *= 3
+print (appel)
+
+appel = 100
+appel /= appel
+print (appel)
