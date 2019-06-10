693a431887ed892ea1547fb9efe679b99c931a3c
Ludelaplu <lucien.koot@gmail.com>
2019-06-10 08:55:31 +0200

    excersise 402.

---
+++ blz48 exc2.py
@@ -0,0 +1,11 @@
+# de oppervlakte van een straal
+
+pi = 3.14159
+straal = 1.00 #  meter
+oppervlakte = (straal * straal * pi)
+print (oppervlakte)
+
+pi = 3.14159
+straal = 1.00
+print( "De oppervlakte van een cirkel met straal", 
+    straal, "is", pi * straal * straal )
